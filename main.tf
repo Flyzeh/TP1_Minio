dcc3243c66fcbe03d748f47044ed604e0eee5855
Flyzeh <giani.negroni@outlook.fr>
2025-11-14 09:01:20 +0100

    refactor(terraform): externalize MinIO config into variables.tf and terraform.tfvars

--- main.tf
+++ main.tf
@@ -1,21 +1,24 @@
 provider "minio" { 
-  minio_server   = "127.0.0.1:9000" 
-  minio_user     = "minioadmin" 
-  minio_password = "minioadmin" 
-} 
+  minio_server   = var.minio_server
+  minio_user     = var.minio_user
+  minio_password = var.minio_password
+}
+
 resource "minio_s3_bucket" "web_bucket" { 
-  bucket = "webbucket" 
-  acl    = "public-read" 
-} 
+  bucket = var.bucket_name
+  acl    = "public-read"
+}
+
 resource "minio_s3_object" "index_html" { 
-  bucket = minio_s3_bucket.web_bucket.bucket 
-  object = "index.html" 
-  source = "index.html" 
-  acl    = "public-read" 
-} 
+  bucket = minio_s3_bucket.web_bucket.bucket
+  object = "index.html"
+  source = "index.html"
+  acl    = "public-read"
+}
+
 resource "minio_s3_object" "style_css" { 
-bucket = minio_s3_bucket.web_bucket.bucket 
-object = "style.css" 
-source = "style.css" 
-acl    = "public-read" 
-} 
+  bucket = minio_s3_bucket.web_bucket.bucket
+  object = "style.css"
+  source = "style.css"
+  acl    = "public-read"
+}
